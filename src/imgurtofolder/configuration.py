a1ef690244ede5a14a621d87ca2061af35ab4b88
Derek Santos <derek@dyrenex.com>
2023-04-02 14:21:33 -0400

    Refactoring configuration.py

--- src/imgurtofolder/configuration.py
+++ src/imgurtofolder/configuration.py
@@ -2,22 +2,12 @@ import json
 import logs
 from os import mkdir
 import os.path
-
-from dataclasses import dataclass
+from pathlib import Path
 
 log = logs.Log('configuration')
 
-@dataclass
-class Configuration:
 
-    config_path: str
-    access_token: str
-    client_id: str
-    client_secret: str
-    download_path: str
-    refresh_token: str
-    overwrite: bool = False
-    max_favorites: int = 30
+class Configuration:
 
     _singleton_instance = None
 
@@ -26,63 +16,82 @@ class Configuration:
             cls._singleton_instance = super(Configuration, cls).__new__(cls, *args, **kwargs)
         return cls._singleton_instance
 
+    def __init__(self,
+                 config_path: str,
+                 access_token: str,
+                 client_id: str,
+                 client_secret: str,
+                 download_path: str,
+                 refresh_token: str,
+                 overwrite: bool = False,
+                 max_favorites: int = 30
+                 ):
+        """
+        Configuration class.
+
+        Parameters:
+            config_path (str): Path to the configuration file.
+            access_token (str): The access token for the API.
+            client_id (str): The client ID for the API.
+            client_secret (str): The client secret for the API.
+            download_path (str): The path to download the images to.
+            refresh_token (str): The refresh token for the API.
+            overwrite (bool): If True, overwrite existing files.
+            max_favorites (int): The maximum number of favorites to download.
+        """
+        self.config_path = config_path
+        self.access_token = access_token
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.download_path = download_path
+        self.refresh_token = refresh_token
+        self.overwrite = overwrite
+        self.max_favorites = max_favorites
+
     def __post_init__(self):
         self.download_path = os.path.realpath(os.path.expanduser(self.download_path))
 
-    def set_access_token(self, token):
-        log.debug('Setting access_token')
-        self._access_token = token
-        self.save_configuration()
+    def __setattr__(self, key, value):
+        """
+        Automatically save the config during a change.
 
-    def set_client_id(self, client_id):
-        log.debug('Setting client_id')
-        self._client_id = client_id
-        self.save_configuration()
-
-    def set_client_secret(self, client_secret):
-        log.debug('Setting client_secret')
-        self._client_secret = client_secret
-        self.save_configuration()
-
-    def set_download_path(self, path):
-        log.debug('Setting download_path')
-        self.download_path = os.path.realpath(os.path.expanduser(path))
-
-    def set_default_download_path(self, path):
-        log.debug('Setting download_path')
-        self.download_path = os.path.realpath(os.path.expanduser(path))
-        self.save_configuration(True)
-
-    def set_refresh_token(self, token):
-        log.debug('Setting refresh token')
-        self._refresh_token = token
+        Not thread safe.
+        """
+        if key == 'download_path':
+            value = os.path.realpath(os.path.expanduser(value))
+        super().__setattr__(key, value)
         self.save_configuration()
 
     def convert_config_to_dict(self, overwrite_download_path=False):
-        log.debug('Converting configuration to json')
-        current_config = {}
-        current_config['access_token'] = self._access_token
-        current_config['client_id'] = self._client_id
-        current_config['client_secret'] = self._client_secret
-        if overwrite_download_path:
-            current_config['download_path'] = self.download_path
-        elif self.get_download_path():
-            current_config['download_path'] = self.get_download_path()
-        current_config['refresh_token'] = self._refresh_token
-        return current_config
+        """
+        Convert the current configuration to a dictionary.
+
+        Parameters:
+            overwrite_download_path (bool): If True, overwrite the download path with the current value.
+        """
+        return {
+            'access_token': self.access_token,
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'download_path': self.download_path if overwrite_download_path else self.download_path,
+            'refresh_token': self.refresh_token
+        }
 
     def save_configuration(self, overwrite_download_path=False):
+        """
+        Save the current configuration to the config file.
+
+        Parameters:
+            overwrite_download_path (bool): If True, overwrite the download path with the current value.
+        """
         log.debug('Saving configuration')
         config_dict = self.convert_config_to_dict(overwrite_download_path)
 
-        if self._config_path[-len('.json'):]:
-            folder_path = self._config_path[:self._config_path.rfind('/')]
-        else:
-            folder_path = self._config_path
+        _path = Path(self.config_path)
 
-        if not os.path.exists(folder_path):
+        if not _path.parent.exists():
             log.debug('Creating config directory')
-            os.makedirs(folder_path)
+            _path.parent.mkdir(parents=True, exist_ok=True)
 
-        with open(self._config_path, 'w') as current_file:
+        with _path.open('w') as current_file:
             json.dump(config_dict, current_file, sort_keys=True, indent=4)
